82ba2d725197650058592bd9ae5f9f79cd9de559
nengrafi <nangrapigood@gmail.com>
2026-09-15 22:07:42 +0900

    fix:곱셈함수

--- src/main.py
+++ src/main.py
@@ -1,11 +1,4 @@
-print("Hello, world!")
-
-def multiply_function(*args):
-    result = 1
-
-    for num in args:
-        result *= args
-
-    return result
 def add(a: int, b: int) -> int:
     return a + b
+def multiply_function(a,b):
+    return a*b
